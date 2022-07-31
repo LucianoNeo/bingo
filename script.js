735eaac39402194f2c217e91428a0ae871dd7b09
LucianoNeo <tec.info.luciano@hotmail.com>
2022-07-31 15:26:56 -0300

    max height bolas

--- script.js
+++ script.js
@@ -60,3 +60,16 @@ numeroSorteado.addEventListener("keypress", function (event) {
         document.getElementById("selecionar").click();
     }
 });
+
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
